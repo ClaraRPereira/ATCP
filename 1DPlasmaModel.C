cc19b203198db2202ad9c4a69921a8a1a500c80a
Clara <clara.pereira@tecnico.ulisboa.pt>
2017-09-30 23:28:33 +0100

    Added method to write system energies into a text file

--- 1DPlasmaModel.C
+++ 1DPlasmaModel.C
@@ -1,5 +1,3 @@
-
-
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
@@ -26,12 +24,17 @@ public:
    
    int NPart;                       // No of particles
    particles part;                  // Initializing one set of particles
-   double dt, tmin, t, tmax;           // Time grid
+   double dt, tmin, tmax;           // Time grid
+   double t;                        // Time variable (evolving time)  
    vector<double> mass(NPart);      // Vector for storing particle masses
    vector<double> pos(NPart);       // Positions
    vector<double> vel(NPart);       // Velocities
-   FILE *data;                      // Create a file to store the trajectories
+   FILE *data, *energies;                      // Create files to store the particle data and the energies
    double Vt;                       // Max velocity for uniform distribution   
+   double E_kin;			        //variáveis para imprimir as energias
+   double E_pot;
+   double E_tot;
+   double L=4;  //size of box
 
 vector<double> particles::dir_product (vector<double> a, vector<double> b) // Defining direct product
 {
@@ -44,15 +47,23 @@ vector<double> particles::dir_product (vector<double> a, vector<double> b) // De
   return product;
 } 
 
-// Write the positions of the particles in the file " data ".
+// Write the positions, velocities and momenta of the particles in the file " DATA ".
 void record_trajectories( )
 {
-  int i;
+  int n=NPart;
 
-  for ( i=0 ; i<part.x.size() ; i++ )
+  for (int i=0 ; i<n ; i++ )
     {
-      fprintf( data, "%f %f %f %f\n", tmin, part.x[i], part.vx[i], part.mx[i] ); // Escrever os valores para o ficheiro "DATA"
+      fprintf( data, "%f %f %f %f\n", t, part.x[i], part.vx[i], part.mx[i] ); // Escrever os valores para o ficheiro "DATA"
     }
+}
+
+// Write the energies of the system in the file " ENERGY ".
+void record_energies()
+{
+
+	fprintf( energies, "%f, %f, %f, %f\n",t, E_kin, E_tot, E_pot);
+
 }
 
 void particles::set_values (vector<double> position, vector<double> velocity, vector<double> mass) {  // Setting positions, velocities and momenta
@@ -67,14 +78,15 @@ void initial_conditions() // GOnna Define the initial COnditions
   vector<double> old_x;
   vector<double> old_vx;
   double m=1; //Let's start by defining all masses as 1
-  double sigma=0.5, n0=0.7; //Valores aleatórios para a carga por unidade de área, sigma, e para a density of neutralizing background charges 
-  double Wp= 4*M_PI*sigma*sigma*n0/m; //Plasma Frequency
+  //double sigma=0.5, n0=0.7; //Valores aleatórios para a carga por unidade de área, sigma, e para a density of neutralizing background charges 
+  //double Wp= 4*M_PI*sigma*sigma*n0/m; //Plasma Frequency
   double r1;     // auxiliary variable to generate a random
   int i;
   double axis=0; // Start of x axis
   double spc=1; //defining intersheet spacing
+  int n=NPart;
   
-  for ( i=0 ; i<NPart ; i++ ) 
+  for ( i=0 ; i<n ; i++ ) 
     {
 
    //Defining the x positions 	
@@ -102,7 +114,7 @@ void initial_conditions() // GOnna Define the initial COnditions
     }  
  */
 
-  for ( i=0 ; i<part.x.size(); i++ )
+  for ( i=0 ; i<n; i++ )
     {
       old_x.push_back(pos[i]) ;  // Guardar valores da posiçao e velocidade no tempo t para quando recalcularmos
       old_vx.push_back(vel[i]);  // tudo com o novo timestep (usando o tc1 e mais tarde o tc2 em vez do delta t inicial)
@@ -130,6 +142,7 @@ void energy( double time )
   double E0= 4*M_PI*sigma*n0; // Amplitude campo Eléctrico
   double xij, xij2;
   int i, j;
+  int n=NPart;
 
   // Kinetic energy of the system
   /*kinetic = 0.0;
@@ -137,14 +150,14 @@ void energy( double time )
     {
       kinetic = kinetic + 0.5 * part.mx[i]*part.vx[i];
     }*/
-  for (i = 0; i < NPart; ++i)
+  for (i = 0; i < n; ++i)
   {
   	 kinetic = 0.5*part.dir_product(part.mx,part.vx)[i];
   }
  
   // Potential Energy of the system
   potential = 0.0;
-  for ( i=0 ; i<NPart ; i++ )
+  for ( i=0 ; i<n ; i++ )
     {
       for ( j=i+1 ; j<NPart ; j++  )       {
             xij = part.x[i] - part.x[j];
@@ -162,6 +175,9 @@ void energy( double time )
   // Print the energies and current timestep
     cout << " " << time << "    "  << kinetic << "    "  << potential << "    "  << etotal  << endl;
  
+ E_kin=kinetic;
+ E_pot=potential;
+ E_tot=etotal;
 
 
 }
@@ -174,6 +190,7 @@ int main()
     	
    // Number of particles
    NPart = 5;
+   int n=NPart;
    // Max velocity
    Vt=4;
 
@@ -183,10 +200,12 @@ int main()
    dt = 0.5;
    t = tmin; // Initial time
 
-   int print_trajectory=1;
+   int print=0; // Variable to decide if i want to pront stuff
 
-   // Create the file to store the trajectories
+   // Create the files to store the data
    data = fopen( "DATA", "w" );
+   energies = fopen( "ENERGY", "w" );
+
 
    //mass.reserve(NPart);
 
@@ -195,7 +214,7 @@ int main()
      // Print, to the terminal, positions, velocities & momenta.
     cout << " Nº Partículas:  " << NPart << " \n " << endl;
 
-    for (int i = 0; i < part.x.size(); ++i)
+    for (int i = 0; i < n; ++i)
     {
     	cout << " Partícula " << i << " \t Position : " << part.x[i] << " |  Velocity :  " ;
     	cout << part.vx[i] << "   |  Momentum : " << part.mx[i] << " |" << endl;
@@ -211,10 +230,10 @@ int main()
       //func();
       // Go to the next timestep
       t = t + dt;
-      tmin += dt;
       // Write the positions of the particles in the file "DATA" if print_trajectory ==1.
-      if (print_trajectory == 1)
+      if (print == 1){
          record_trajectories( );
+         record_energies( );}
 
       // Compute energies at current timestep
       energy(t);
@@ -224,6 +243,7 @@ int main()
 
   // Close trajectories file
   fclose( data );
+  fclose( energies );
 	
 
 	return 0;
